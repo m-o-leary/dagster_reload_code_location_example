b18fae6fb41a617dcc3d90676b95f1a7da741b41
m-o-leary <moleary1987@gmail.com>
2025-07-08 08:37:29 +0100

    add defs

--- definitions.py
+++ definitions.py
@@ -1,26 +1,38 @@
 import json
+from typing import Any
 import dagster as dg
-import pathlib  
+import pathlib
 from lib.sensors import json_file_reload_sensor
+
+
 # Asset factory function
 def create_data_asset(name: str, source_table: str):
-    @dg.asset(name=name)
-    def _asset(context: dg.AssetExecutionContext):
+    @dg.asset(
+        name=name,
+        ins={"data": dg.AssetIn(  "raw_transactions")},
+        kinds={"s3"},
+        automation_condition=dg.AutomationCondition.any_deps_updated(),
+    )
+    def _asset(context: dg.AssetExecutionContext, data: Any):
         context.log.info(f"Processing {source_table}")
-        # Your data processing logic here
         return f"Data from {source_table}"
-    
+
     return _asset
 
+
 # Generate assets using the factory
-asset_configs = json.load((pathlib.Path(__file__).parent / "assets.json").resolve().open())  # noqa: F821
+asset_configs = json.load(
+    (pathlib.Path(__file__).parent / "assets.json").resolve().open()
+)  # noqa: F821
 
 generated_assets = [
-    create_data_asset(config["name"], config["source_table"]) 
+    create_data_asset(config["name"], config["source_table"])
     for config in asset_configs
 ]
 
+external_assets = [dg.AssetSpec("raw_transactions")]
+
 defs = dg.Definitions(
-    assets=generated_assets,
+    assets=generated_assets + external_assets,
     sensors=[json_file_reload_sensor],
 )
